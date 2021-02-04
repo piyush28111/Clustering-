0bcdc2a68fcd395c7717b8e06a0ec43e83cff0c0
Piyush <57325505+piyush28111@users.noreply.github.com>
2021-02-04 19:29:06 +0530

    Update Apriori.py

--- Apriori.py
+++ Apriori.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 21 17:21:39 2020
-
-@author: Lenovo
-"""
 
 from efficient_apriori import apriori
 
@@ -69,25 +63,7 @@ print(lift[['antecedents','consequents','support','confidence','lift']])
 
 
 
-
-
-
-
-
-
-
 # 3 _ AR 
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -163,89 +139,3 @@ lift[['antecedents','consequents','support','confidence','lift']].head()
 lift[(lift.confidence>0.5) & (lift.lift>12)]
 
 
-# 6_mbasket1 
-
-
-import pandas as pd 
-import numpy as np 
-from mlxtend.frequent_patterns import apriori 
-from mlxtend.frequent_patterns import association_rules
-
-df = pd.read_csv('online_store.csv')
-df.columns
-df.head()
-
-df['Description']
-
-data = df.copy()
-
-data.head()
-data.isnull().any(axis=0)
-data.dropna()
-data
-data.dropna(axis=0,inplace=True)
-data.dropna(axis=0,inplace=True,subset=['Description'])
-data
-
-data['Description']=data['Description'].str.strip()
-data['Description']
-
-data.dtypes
-data['InvoiceNo'].astype('str')
-data.set_index('index')
-    
-data = data[~data['InvoiceNo'].str.contains('C')]
-data
-
-basket = df[df['Country']=='United Kingdom'].groupby(['InvoiceNo','Description'])['Quantity']
-basket = basket.sum().unstack().reset_index().fillna(0).set_index('InvoiceNo')
-basket.head()
-
-basket.to_csv('checkingbasket.csv')
-
-basket.head()
-
-
-def encode_units(x):
-    if x <= 0:
-        return 0
-    if x >= 1:
-        return 1
-basket_sets = basket.applymap(encode_units)
-basket_sets.head()
-
-
-basket_sets.values
-basket_sets.columns
-
-
-basket_sets.drop('POSTAGE', inplace=True, axis=1)
-
-frq_items = apriori(basket_sets,min_support=0.03,use_colnames=True)
-frq_items
-frq_items.to_csv('frq_items.csv')
-
-
-rules= association_rules(frq_items,metric='lift',min_threshold=1)
-rules[['antecedents','consequents','confidence','support','lift']]
-
-rules[ (rules['lift'] >= 6) &  (rules['confidence'] >= 0.8) ]
-
-basket['ALARM CLOCK BAKELIKE GREEN'].sum()
-
-basket['ALARM CLOCK BAKELIKE RED'].sum()
-
-
-
-
-'''
-from mlxtend.preprocessing import TransactionEncoder
-te= TransactionEncoder()
-te_ar = te.fit(basket_sets).transform(basket_sets)
-te.columns_
-
-df = pd.DataFrame(te_ar,columns=te.columns_)
-df
-
-apriori(df,min_support=0.03,use_colnames=(True))
-'''
